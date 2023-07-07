122244c1e5747a4fbce112e60598bedd54baa7ad
Atharva Inamdar <aainamdar.cse@gmail.com>
2023-07-07 21:45:15 +0530

    css changes

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ function createNoteElement(id, content) {
 
   element.classList.add("note");
   element.value = content;
-  element.placeholder = "Empty Sticky Note";
+  element.placeholder = "Empty Note 📝:";
 
   element.addEventListener("change", () => {
     updateNote(id, element.value);
